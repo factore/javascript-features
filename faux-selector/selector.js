2d986ae53a3cbe0266e8ea34046d6a36316729fd
Jamie Allen <talk.to.jme@gmail.com>
2012-05-30 13:08:24 -0400

    faux-select drop down so that you can style it as you like.

---
+++ faux-selector/selector.js
@@ -0,0 +1,22 @@
+// ass seen here: http://habiamobile-com.factorepreview.ca/
+
+APP.features.selector = {
+  init: function (options) {
+    $fauxSelect = $('.faux-select');
+    $select = $('.select-box select');
+
+    $fauxSelect.on('click', '.click', function () {
+      $fauxSelect.find('ul.options').slideToggle(100);
+    });
+
+    $fauxSelect.on('click', '.value', function () {
+      $fauxSelect.find('ul.options').slideToggle(100);
+    });
+
+    $fauxSelect.on('click', '.option', function () {
+      $select.val($(this).attr('data')).trigger('change');
+      $fauxSelect.find('.value').html($(this).html());
+      $fauxSelect.find('ul.options').slideUp(100);
+    });
+  }
+};
